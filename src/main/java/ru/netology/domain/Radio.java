fdee21c53696c939255c2121b3e69528aac81198
TanyTany <avdtatyana@gmail.com>
2020-12-26 17:07:54 +0300

    added tests

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -3,13 +3,46 @@ package ru.netology.domain;
 public class Radio {
 
         private int currentStation;
-        private int volume;
+        int firstStation = 0;
+        int lastStation = 9;
 
 
-        public int changeStation(){
+        private int currentVolume;
+        int firstVolume = 0;
+        int lastVolume = 10;
 
+
+        public int changeNextStation(){
                 currentStation +=1;
+                if (currentStation > lastStation){
+                        return firstStation;
+                }
                 return currentStation;
+        }
+
+        public int changePrevStation(){
+                currentStation -= 1;
+                if (currentStation < firstStation){
+                        return lastStation;
+                }
+                return currentStation;
+        }
+
+        public int changeNextVolume(){
+                currentVolume +=1;
+                if (currentVolume > lastVolume){
+                        return lastVolume;
+                }
+                return currentVolume;
+        }
+
+        public int changePrevVolume(){
+                currentVolume -=1;
+                if (currentVolume < firstVolume){
+                        return firstVolume;
+
+                }
+                return currentVolume;
         }
 
         public int getCurrentStation() {
@@ -21,12 +54,20 @@ public class Radio {
                 this.currentStation = currentStation;
         }
 
-        public int getVolume() {
-                return volume;
+        public int getCurrentVolume() {
+                return currentVolume;
         }
 
-        public void setVolume(int volume) {
-                this.volume = volume;
+        public void setCurrentVolume(int currentVolume) {
+
+//                if (currentVolume > lastVolume){
+//                        return;
+//                }
+//                if (currentVolume < firstVolume){
+//                        return;
+//                }
+
+                this.currentVolume = currentVolume;
         }
 
 
--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -5,24 +5,105 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class RadioTest {
 
-    /// проверочный тест
+    /// проверочные тесты
     @Test
     public void shouldChangeStation() {
         Radio radio = new Radio();
         assertEquals(0, radio.getCurrentStation());
-        radio.setCurrentStation(20);
-        assertEquals(20, radio.getCurrentStation());
+        radio.setCurrentStation(9);
+        assertEquals(9, radio.getCurrentStation());
+
+    }
+
+    @Test
+    public void shouldChangeVolume() {
+        Radio radio = new Radio();
+        assertEquals(0, radio.getCurrentVolume());
+        radio.setCurrentVolume(6);
+        assertEquals(6, radio.getCurrentVolume());
 
     }
  /// вызов метода должен приводить к переключению на одну радиостанцию
     @Test
-    public void shouldChangeStation2() {
+    public void shouldNextStationWithLastStation() {
         Radio radio = new Radio();
-        int expected = 1;
-        int actual = radio.changeStation();
+        radio.setCurrentStation(9);
+        int expected = 0;
+        int actual = radio.changeNextStation();
         assertEquals(expected, actual);
 
+    }
+    @Test
+    public void shouldNextStationWithFirstStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(0);
+        int expected = 1;
+        int actual = radio.changeNextStation();
+        assertEquals(expected, actual);
 
     }
+
+    @Test
+    public void shouldPrevStationWithLastStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(0);
+        int expected = 9;
+        int actual = radio.changePrevStation();
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldPrevStationWnithFirstStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(7);
+        int expected = 6;
+        int actual = radio.changePrevStation();
+        assertEquals(expected, actual);
+
+    }
+
+    ///Клиент должен иметь возможность увеличивать и уменьшать уровень громкости звука
+    @Test
+    public void shouldNextVolumeWhithLastStation() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(10);
+        int expected = 10;
+        int actual = radio.changeNextVolume();
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldNextVolumeWhithFirstStation() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(0);
+        int expected = 1;
+        int actual = radio.changeNextVolume();
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldPrevVolumeWithLastStation() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(10);
+        int expected = 9;
+        int actual = radio.changePrevVolume();
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldPrevVolumeWithFirstStation() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(0);
+        int expected = 0;
+        int actual = radio.changePrevVolume();
+        assertEquals(expected, actual);
+
+    }
+
+
 }
 
